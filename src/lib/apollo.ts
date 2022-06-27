b1519f6f83d02350f998dd263d39156ecda1f439
Fernanda <fernandaos12@gmail.com>
2022-06-26 22:46:12 -0300

    Page Subscribe

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,10 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-    uri: 'https://api-sa-east-1.graphcms.com/v2/cl4rrza401jv501z63zbp1f64/master',
+   // uri: 'https://api-sa-east-1.graphcms.com/v2/cl4rrza401jv501z63zbp1f64/master',
+  uri: import.meta.env.VITE_API_URI,
+   headers:{
+    "Authorization": `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+   },
     cache: new InMemoryCache()
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      backgroundImage:{
+        bgblur: 'url(/src/assets/bg_dark.png)'
+      },
  fontFamily:{
   sans:'Roboto, sans-serif'
  },     
